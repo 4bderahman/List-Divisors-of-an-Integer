f8b7429edfc7487cfe4eb807380ac583377762c0
4bderahman <146900899+4bderahman@users.noreply.github.com>
2023-10-09 02:08:31 +0100

    Add files via upload

---
+++ Exercice27.py
@@ -0,0 +1,9 @@
+# intering the integer
+n = int(input("Enter a positive non-zero integer: "))
+
+print(f"The divisors of {n} are:")
+
+# Loop through all possible divisors
+for i in range(1, n + 1):
+    if n % i == 0:  # If i is a divisor of n
+        print(i)
